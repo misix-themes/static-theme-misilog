83c4db9c397c653a8ed077e1d218e49b6e94bb59
misitebao <i@misitebao.com>
2021-08-24 18:27:09 +0800

    feat: increase the project directory after the build

---
+++ dist/static/js/about.js
@@ -0,0 +1,1 @@
+import"./dynamic-import-polyfill.js";Vue.createApp({setup:p=>(console.log("setup"),{})}).mount("#app");
---
+++ dist/static/js/home.js
@@ -0,0 +1,1 @@
+import"./dynamic-import-polyfill.js";/* empty css     */Vue.createApp({setup:p=>(console.log("setup"),{})}).mount("#app");
